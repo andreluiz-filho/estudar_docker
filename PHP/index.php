4975b77a118af1fa4d2ebf5f2105b9908fe6dadb
andreluiz-filho <andreluiz.melofilho@gmail.com>
2022-12-07 16:50:45 -0300

    .

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pagina PHP</title>
+	</head>
+	<body>
+		<h1>Escreva a Mensagem (AGORA)</h1>
+
+		<form action='process.php' method='POST'>
+			<input type='text' name='message' id='message'>
+			<input type='submit' value='Enviar Mensagem'>
+		</form>
+
+	</body>
+</html>
